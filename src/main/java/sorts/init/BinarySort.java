269fe62785e264b00af59ea32e54d6f6fb21e87a
Tim <vtimofej@mail.ru>
2024-09-11 01:16:04 +0300

    вставка объяснения

---
+++ src/main/java/sorts/init/BinarySort.java
@@ -0,0 +1,20 @@
+package sorts.init;
+
+import sorts.Sort;
+
+import java.util.List;
+
+public class BinarySort extends Sort {
+    public BinarySort(List<Integer> array) {
+        super(array);
+    }
+
+    public BinarySort(int size, int bound) {
+        super(size, bound);
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
